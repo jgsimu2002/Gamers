c9bb6bd739a055666cb2c31d4033c8ec49805592
Usuario <Usuario@Dusker-256GB>
2022-02-03 15:12:17 -0600

    mi primer comic del proyecto

---
+++ src/test/java/com/gamer/gamer/GamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamer.gamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
